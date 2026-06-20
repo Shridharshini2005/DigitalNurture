a55eb9d39c71df37a42aca4c6f53edb5dee386ef
Shridharshini2005 <164712674+Shridharshini2005@users.noreply.github.com>
2026-06-20 21:52:39 +0530

    Add files via upload

---
+++ Week1/Product.java
@@ -0,0 +1,17 @@
+package ECommercePlatformSearchFunction;
+
+public class Product {
+
+    int productId;
+    String productName;
+    String category;
+
+    public Product(int productId,
+                   String productName,
+                   String category) {
+
+        this.productId = productId;
+        this.productName = productName;
+        this.category = category;
+    }
+}
