31b375ddce5ddffca1d7d939dfe09def8fdbdc7f
Shridharshini2005 <164712674+Shridharshini2005@users.noreply.github.com>
2026-06-21 13:09:04 +0530

    Add files via upload

---
+++ Week1/Scenario1.sql
@@ -0,0 +1,16 @@
+BEGIN
+    FOR cust IN (
+        SELECT CustomerID
+        FROM Customers
+        WHERE FLOOR(MONTHS_BETWEEN(SYSDATE, DOB)/12) > 60
+    )
+    LOOP
+        UPDATE Loans
+        SET InterestRate = InterestRate - 1
+        WHERE CustomerID = cust.CustomerID;
+    END LOOP;
+
+    COMMIT;
+END;
+/
+SELECT * FROM Loans;
