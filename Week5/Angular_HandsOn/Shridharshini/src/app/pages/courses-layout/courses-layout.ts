eefd0db1ed1ef942b4cd875a2972ecea1e8c8155
Shridharshini2005 <164712674+Shridharshini2005@users.noreply.github.com>
2026-07-19 21:50:41 +0530

    Add files via upload

---
+++ Week5/Angular_HandsOn/Shridharshini/src/app/pages/courses-layout/courses-layout.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-courses-layout',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './courses-layout.html',
+})
+export class CoursesLayout {}
